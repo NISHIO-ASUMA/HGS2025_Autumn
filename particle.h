4bbf56f50cb0babae15e0097416494cfae918b09
種川陸 <241312@st.yoshida-g.ac.jp>
2025-09-29 14:22:06 +0900

    エフェクト、パーティクルの追加

---
+++ particle.h
@@ -0,0 +1,173 @@
+//=============================================================================
+//
+// パーティクル処理 [particle.h]
+// Author : RIKU TANEKAWA
+//
+//=============================================================================
+#ifndef _PARTICLE_H_// このマクロ定義がされていなかったら
+#define _PARTICLE_H_// 2重インクルード防止のマクロ定義
+
+//*****************************************************************************
+// インクルードファイル
+//*****************************************************************************
+#include "main.h"
+#include "effect.h"
+
+// 前方宣言
+class CParticle;
+
+//*****************************************************************************
+// パーティクルクラス
+//*****************************************************************************
+class CParticle : public CEffect
+{
+public:
+	CParticle();
+	~CParticle();
+
+	//*************************************************************************
+	// パーティクル生成テンプレート
+	//*************************************************************************
+	// particleType : 生成したいパーティクルクラス名
+	template <typename particleType>
+	static particleType* Create(D3DXVECTOR3 dir, D3DXVECTOR3 pos, D3DXCOLOR col, int nLife, int nMaxParticle)
+	{
+		// 渡された型がCParticleを継承していなかった場合
+		static_assert(std::is_base_of<CParticle, particleType>::value, "型はCParticleを継承していません。");
+
+		particleType* pParticle = new particleType();// 型のインスタンス生成
+
+		if (!pParticle)
+		{
+			return nullptr;
+		}
+
+		// 初期化処理
+		pParticle->Init();
+
+		if (dir != VECTOR3_NULL)
+		{
+			pParticle->SetDir(dir);
+		}
+
+		pParticle->SetPos(pos);
+		pParticle->SetCol(col);
+		pParticle->SetLife(nLife);
+		pParticle->m_nMaxParticle = nMaxParticle;
+
+		return pParticle;
+	}
+
+	HRESULT Init(void);
+	void Update(void);
+
+	void SetLife(int nLife) { m_nLife = nLife; }
+	void SetDir(D3DXVECTOR3 dir) { m_Dir = dir; }
+
+	int GetLife(void) { return m_nLife; }
+	int GetMaxParticle(void) { return m_nMaxParticle; }
+	D3DXVECTOR3 GetDir(void) { return m_Dir; }
+
+private:
+	D3DXVECTOR3 m_Dir;	// 方向
+	int m_nLife;		// 寿命
+	int m_nMaxParticle;	// 粒子の数
+};
+
+//*****************************************************************************
+// 炎パーティクルクラス
+//*****************************************************************************
+class CFireParticle : public CParticle
+{
+public:
+	CFireParticle();
+	~CFireParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+//*****************************************************************************
+// 水しぶきパーティクルクラス
+//*****************************************************************************
+class CWaterParticle : public CParticle
+{
+public:
+	CWaterParticle();
+	~CWaterParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+//*****************************************************************************
+// オーラパーティクルクラス
+//*****************************************************************************
+class CAuraParticle : public CParticle
+{
+public:
+	CAuraParticle();
+	~CAuraParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+//*****************************************************************************
+// 移動時パーティクルクラス
+//*****************************************************************************
+class CMoveParticle : public CParticle
+{
+public:
+	CMoveParticle();
+	~CMoveParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+//*****************************************************************************
+// 浮遊パーティクルクラス
+//*****************************************************************************
+class CFloatingParticle : public CParticle
+{
+public:
+	CFloatingParticle();
+	~CFloatingParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+//*****************************************************************************
+// 水流(排水)パーティクルクラス
+//*****************************************************************************
+class CWaterFlowParticle : public CParticle
+{
+public:
+	CWaterFlowParticle();
+	~CWaterFlowParticle();
+
+	HRESULT Init(void);
+	void Update(void);
+
+private:
+
+};
+
+#endif
